1e9ee209354706cd561b47d9ba4a85feddf7b17e
Rostyslav Hundarev <r.hundarev.im21@kpi.ua>
2024-06-02 23:34:49 +0300

    add test getHealthyServers

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,71 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	. "gopkg.in/check.v1"
+)
 
 func TestBalancer(t *testing.T) {
 	// TODO: Реалізуйте юніт-тест для балансувальникка.
+	// Загалом, в Test_getServer імплементований загальний юніт-тест для балансувальника.
+	// Я навіть зловив на ньому помилку, коли сервер брався не з списку здорових серверів, а із загального списку
+	TestingT(t)
+}
+
+type BalancerSuite struct{}
+
+var _ = Suite(&BalancerSuite{})
+
+func (s *BalancerSuite) Test_getHealthyServers(c *C) {
+	serversStates = map[string]bool{}
+	tests := []struct {
+		name              string
+		serversStatesTest map[string]bool
+		wantResult        []string
+	}{
+		{
+			"no servers",
+			map[string]bool{},
+			[]string{},
+		},
+		{
+			"no available servers",
+			map[string]bool{
+				"server1": false,
+			},
+			[]string{},
+		},
+		{
+			"single available server",
+			map[string]bool{
+				"server1": true,
+			},
+			[]string{"server1"},
+		},
+		{
+			"1 available server, 1 unavailable",
+			map[string]bool{
+				"server1": true,
+				"server2": false,
+			},
+			[]string{"server1"},
+		},
+		{
+			"2 unavailable servers",
+			map[string]bool{
+				"server1": false,
+				"server2": false,
+			},
+			[]string{},
+		},
+	}
+	for _, tt := range tests {
+		serversStates = tt.serversStatesTest
+		gotResult := getHealthyServers()
+		if len(gotResult) == len(tt.wantResult) && len(tt.wantResult) == 0 {
+			continue
+		}
+		c.Assert(gotResult, DeepEquals, tt.wantResult)
+	}
 }
