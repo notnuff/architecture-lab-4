2ebe5ed62dd2ca532f0dc585259e8376200b3fed
notnuff <113949291+notnuff@users.noreply.github.com>
2024-06-03 23:21:41 +0300

    made integration test

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -3,28 +3,65 @@ package integration
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"testing"
 	"time"
+
+	. "gopkg.in/check.v1"
 )
 
-const baseAddress = "http://balancer:8090"
+const baseAddress = "http://localhost:8090"
 
 var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
-	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
+func Test(t *testing.T) { TestingT(t) }
+
+type TestBalancerSuite struct{}
+
+var _ = Suite(&TestBalancerSuite{})
+
+func (s *TestBalancerSuite) TestBalancer(c *C) {
+	//if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+	//	t.Skip("Integration test is not enabled")
+	//}
+
+	// DONE: Реалізуйте інтеграційний тест для балансувальникка.
+	// В цьому тесті, на мою думку, варто погратися із математичною статистикою, і, скажімо, подивитися на
+	// кінцеві використання кожного із серверів. Якщо використання кожного із серверів більш-менш рівномірне,
+	// то і балансувальник працює правильно. Хоч це і не ідеально точний тест, проте він достатньо точний
+	// для наших потреб. А ще, це прикольне застосування теорії ймовірностей у теорії програмування)
+
+	serversUses := map[string]int{}
+
+	numOfRequests := 10000
+	for i := 0; i < numOfRequests; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+
+		if err != nil {
+			c.Error(err)
+		}
+
+		usedServer := resp.Header.Get("lb-from")
+		serversUses[usedServer] += 1
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	c.Logf("servers usages: %v", serversUses)
+	numOfServers := len(serversUses)
+	targetServerUsage := 1.0 / float64(numOfServers)
+	acceptableStatisticalError := 0.1 * targetServerUsage
+	acceptableUsagePerServer := targetServerUsage - acceptableStatisticalError
+	c.Logf("acceptable usage per server: %f", acceptableUsagePerServer)
+
+	for s := range serversUses {
+		serverPercentageLoad := float64(serversUses[s]) / float64(numOfRequests)
+		c.Logf("[%s] server percentage usage: %f", s, serverPercentageLoad)
+
+		if serverPercentageLoad <= acceptableUsagePerServer {
+			c.Error("server percentage is too small", s, serverPercentageLoad)
+		}
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
 }
 
 func BenchmarkBalancer(b *testing.B) {
